71235b13f18a92e210c3f155da6ac66e2083a2dc
Rajat Pandey <pandeyrajat9971@gmail.com>
2026-07-24 23:52:44 +0530

    Added Contact Component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './Components/Navbar.jsx';
 import About from './Components/About.jsx';
 import Home from './Components/Home.jsx';
 import Footer from './Components/Footer.jsx';
+import Contact from './Components/Contact.jsx';
 import {Routes,Route} from "react-router-dom";
 import './App.css'
 
@@ -16,8 +17,10 @@ function App() {
         <Navbar />
         <main className="flex-1">
           <Routes>
+            
             <Route path='/' element={<Home/>} />
             <Route path="/about" element = {<About />} />
+            <Route path='/contact' element = {<Contact/>}/>
           </Routes>
         </main>
         
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,9 +2,14 @@ import {Link} from "react-router-dom";
 const Navbar = ({name})=>{
     return <>
         <div className="flex justify-between bg-gray-800 text-white p-4">
-            <h1 className="bg-gray-600 p-2 rounded-xl ">Git & Github</h1>
-            <h1 className="bg-gray-600 p-2 rounded-xl">Learning From Shreyash coding school</h1>
-            <Link to='/about' className="bg-gray-600 p-2 rounded-xl">About this page</Link>
+            <Link to='/' className="bg-gray-600 p-2 rounded-xl ">Git & Github</Link>
+            <h1 className=" p-2 rounded-xl text-green-400">Learning From Shreyash coding school</h1>
+
+            <div className='flex gap-2'>
+                 <Link to='/about' className="bg-gray-600 p-2 rounded-xl px-4">About</Link>
+                <Link to='/contact' className="bg-gray-600 p-2 rounded-xl px-4">Contact</Link>
+        
+            </div>    
         </div>
     </>
 }
