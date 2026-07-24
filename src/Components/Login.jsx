9a48512a976cb646431aee79c2cbf8d5c39e028b
Rajat Pandey <pandeyrajat9971@gmail.com>
2026-07-24 21:32:11 +0530

    update login page

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,8 +1,10 @@
 const login = ()=>{
     return <>
         <h1>This is the login page</h1>
-        <div>Form started</div>
-        <div>This is the first div</div>
+        <form>
+            hey
+        </form>
+        
     </>
 }
 
